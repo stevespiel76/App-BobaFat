ee15c9f3fc9ccdd201217fcbcfa2eaea1a5ab975
stevespiel76 <stevespiel76@gmail.com>
2025-10-03 20:52:33 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import HomeScreen from "./HomeScreen";
-import OrderDetailsScreen from "./OrderDetailsScreen";
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import OrderDetailsScreen from './src/screens/OrderDetailsScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false, // hide headers globally
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="OrderDetails" component={OrderDetailsScreen} />
       </Stack.Navigator>
